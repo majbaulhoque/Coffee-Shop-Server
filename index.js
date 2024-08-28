b8cd4db4eba94ad0f5466cd4abd900922d074d5a
Majba UL Hoque <majba9399@gmail.com>
2024-08-28 16:25:15 +0600

    added delete method

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/coffee/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query);
+      res.send(result)
+    })
+
     // create or post method
     app.post('/coffee', async(req, res) =>{
       const newCoffee = req.body;
@@ -45,12 +52,35 @@ async function run() {
       res.send(result)
     })
 
+    // Updated Method
+    app.put('/coffee/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name, 
+          quantity: updatedCoffee.quantity, 
+          supplier: updatedCoffee.supplier, 
+          taste: updatedCoffee.taste, 
+          category: updatedCoffee.category, 
+          details: updatedCoffee.details, 
+          photo: updatedCoffee.photo
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result)
+    })
+
     // Delete Method
     app.delete('/coffee/:id', async (req, res) =>{
       const id = req.params.id;
       console.log(id)
       const query = {_id: new ObjectId(id)};
       const result = await coffeeCollection.deleteOne(query);
+      console.log(result)
       res.send(result)
     })
 
