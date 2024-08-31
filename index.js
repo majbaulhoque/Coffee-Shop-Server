51a6782b233cd39fd643af557d89cbbc54bf8d8f
Majba UL Hoque <majba9399@gmail.com>
2024-08-31 20:29:10 +0600

    delete & get method add in server side for user

--- index.js
+++ index.js
@@ -86,10 +86,23 @@ async function run() {
     })
 
     // User related APIS
+    app.get('/user', async(req, res) =>{
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users)
+    })
+
     app.post('/user', async(req, res) =>{
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user)
+      res.send(result);
+    })
+
+    app.delete('/user/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
       res.send(result)
     })
 
